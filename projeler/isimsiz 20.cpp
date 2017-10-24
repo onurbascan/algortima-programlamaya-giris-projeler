85f4d80de2fae6e1d0f12b4c722968663b001f0b
onurbascan <bascanonur@gmail.com>
2017-10-24 21:22:50 +0300

    yazılım geliştirme

---
+++ projeler/isimsiz 20.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+#include <conio.h>
+
+main()
+{
+	char kelime[20];
+	printf("bir kelime girin \n");
+	scanf("%s",kelime);
+	
+	for(int i=0;i<10;i++)
+	printf("%s \n",kelime);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	getch();
+	
+
+}
